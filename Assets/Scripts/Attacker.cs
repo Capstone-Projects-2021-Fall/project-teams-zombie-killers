8c19ad78b39caaa4a6c0b6c49314268896a33738
Tonmoy <59925709+TonmoyChak@users.noreply.github.com>
2021-10-16 14:16:35 -0400

    Update Attacker.cs

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
+    [Range (0f, 5f)]
+    [SerializeField] float walkSpeed = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class Attacker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * Time.deltaTime);
+        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
     }
 }
