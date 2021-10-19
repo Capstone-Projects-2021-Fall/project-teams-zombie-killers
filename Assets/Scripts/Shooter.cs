04b305c5b658d6635b6d0c414caf29ad34a7a2b2
Xhuljano Racaku <racakuxhuljano@yahoo.com>
2021-10-19 19:42:37 -0400

    Added first defender
    Created first defender with idle and attacking moves. Also, added a projectile that defender throws.

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectile, gun;
+
+    public void Shoot()
+    {
+        Instantiate(projectile, gun.transform.position, transform.rotation);
+    }
+}
