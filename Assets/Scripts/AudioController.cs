47e8ee1a55ae5f7470302839e227f9068fafca31
Francesko Racaku <franceskoracaku@hotmail.com>
2021-10-19 02:26:39 -0400

    Settings Advanced, main menu, username.
    Settings Advanced. Username Added.  Main Menu button added. Save states of music and name implemented

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Profiles m_profiles;
+
+    [SerializeField]
+    private List<SliderX> m_VolumeSliders = new List<SliderX>();
+
+
+    private void Awake()
+    {
+        if (m_profiles != null)
+            m_profiles.SetProfile(m_profiles);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Settings.profile && Settings.profile.audioMixer != null)
+            Settings.profile.GetAudioLevels();
+    }
+
+    public void ApplyChanges()
+    {
+        if (Settings.profile && Settings.profile.audioMixer != null)
+            Settings.profile.SaveAudioLevels();
+    }
+
+    public void CancelChanges()
+    {
+        if (Settings.profile && Settings.profile.audioMixer != null)
+            Settings.profile.GetAudioLevels();
+
+
+        for (int i = 0; i < m_VolumeSliders.Count; i++)
+        {
+            m_VolumeSliders[i].ResetSliderValue();
+        }
+    }
+}
