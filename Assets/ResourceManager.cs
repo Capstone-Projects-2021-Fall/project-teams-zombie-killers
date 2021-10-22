1faec62934b4312b203dba34be118092e69bbf49
Darren Chen <tuj72544@temple.edu>
2021-10-22 04:09:27 -0400

    Resources now display on screen in the GUI and update properly

--- Assets/ResourceManager.cs
+++ Assets/ResourceManager.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ResourceManager : MonoBehaviour
 {
+    public Resource resource;
+    public TextMeshProUGUI resourceDisplay;
+    private float displayCount;
+
+    public static ResourceManager singleton;
+    private void Awake()
+    {
+        if (singleton == null)
+        {
+            singleton = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        displayCount = resource.GetCount();
+        StartCoroutine(RefreshDisplay());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator RefreshDisplay()
     {
-        
+        while (true)
+        {
+            if (displayCount != resource.GetCount())
+            {
+                displayCount = resource.GetCount();
+                resourceDisplay.text = "Resources: " + displayCount;
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
     }
 }
