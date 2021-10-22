cea97638f18521a8fc334e4c87411465feded9cf
Xhuljano Racaku <racakuxhuljano@yahoo.com>
2021-10-21 22:46:20 -0400

    Added defender selection UI
    Added defender selection UI where the user can click the defender they want to place. Also added a second defender.

---
+++ Assets/Scripts/UI/DefenderButton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+
+{
+    [SerializeField] Defenders defenderPrefab;
+
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<DefenderButton>();
+        
+        foreach(DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(41, 41, 41, 255);
+        }
+        
+        GetComponent<SpriteRenderer>().color = Color.white;
+        FindObjectOfType<DefenderSpawner>().SetSelectedDefender(defenderPrefab);
+    }
+}
---
+++ Assets/Scripts/UI/DefenderSpawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    Defenders defender;
+
+    private void OnMouseDown()
+    {
+        SpawnDefender(GetSquareClicked());
+    }
+
+    public void SetSelectedDefender(Defenders defenderToSelect)
+    {
+        defender = defenderToSelect;
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return gridPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
+
+    }
+
+    private void SpawnDefender(Vector2 roundedPos)
+    {
+        Defenders newDefender = Instantiate(defender, roundedPos, Quaternion.identity) as Defenders;
+    }
+
+}
