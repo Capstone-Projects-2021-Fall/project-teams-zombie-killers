b872205f934886070d1eccef8904604aab21b510
Tonmoy <59925709+TonmoyChak@users.noreply.github.com>
2021-10-29 19:15:33 -0400

    Sunflower

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tower : MonoBehaviour
+public class Defender : MonoBehaviour
 {
+
     [SerializeField] int starCost = 100;
+
+    public int GetStarCost()
+    {
+        return starCost;
+    }
 
     public void AddStars(int amount)
     {
         FindObjectOfType<StarDisplay>().AddStars(amount);
     }
+
 }
