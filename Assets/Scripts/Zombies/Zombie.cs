ec1c1e5d511633d7c6f5b8c77a64d7f46066772a
Marcus-Anestad <tuj23850@temple.edu>
2021-10-29 21:54:12 -0400

    Update Zombie.cs

--- Assets/Scripts/Zombies/Zombie.cs
+++ Assets/Scripts/Zombies/Zombie.cs
@@ -30,7 +30,7 @@ public class Zombie : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = startingHealth;
     }
 
     // Update is called once per frame
