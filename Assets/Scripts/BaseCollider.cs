04c6eb699fa447b447b99da318f0dacc37bc4a21
Xhuljano Racaku <racakuxhuljano@yahoo.com>
2021-11-04 22:31:34 -0400

    Fixed a bug on win condition
    Now win will work even if 1 or 2 zombies will go all the way to the left to the base

--- Assets/Scripts/BaseCollider.cs
+++ Assets/Scripts/BaseCollider.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class BaseCollider : MonoBehaviour
 {
-    private void OnTriggerEnter2D()
+    private void OnTriggerEnter2D(Collider2D otherCollider)
     {
         FindObjectOfType<Lives>().TakeLife();
+        Destroy(otherCollider.gameObject);
     }
 }
