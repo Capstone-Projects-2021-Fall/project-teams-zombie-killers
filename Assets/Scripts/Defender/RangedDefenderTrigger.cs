85c50180783f3a8784d360a49e2e348e8a2206b4
Marcus-Anestad <tuj23850@temple.edu>
2021-11-12 14:43:37 -0500

    Defender Attack now has range implemented
    Defender will now only shoot defenders within a given range. If the projectile would move out of that range then it is destroyed.

---
+++ Assets/Scripts/Defender/RangedDefenderTrigger.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedDefenderTrigger : MonoBehaviour
+{
+    [Header("Projectile shot by the ranged defender")]
+    public GameObject projectilePrefab;
+
+    [Header("Damage that will be dealt by projectile")]
+    [SerializeField] private int damage;
+
+    [Header("The defender will only shoot defenders in this range.")]
+    [SerializeField] private int range;
+
+    [Header("How many seconds the defender waits until it shoots again.")]
+    [SerializeField] private float shootSpeed;
+
+
+    private int zombiesInRange = 0;
+
+
+    void Awake()
+    {
+        projectilePrefab.GetComponent<Projectile>().damage = damage;
+        projectilePrefab.GetComponent<Projectile>().range = range;
+
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        collider.size = new Vector2( (range+1) * 1.2f, 2.4f);
+        collider.offset = new Vector2( range * 0.6f, 0);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Checks that it was an Enemy that entered the 2D Collier
+        //Then checks that there wasn't already a zombie in the range
+        if(collision.CompareTag("Enemy") && zombiesInRange++ == 0)
+            StartCoroutine(repeatedShootProjectile());
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+            zombiesInRange -= 1;
+    }
+
+
+    #region Shoot Functions
+    IEnumerator repeatedShootProjectile()
+    {
+        while (zombiesInRange > 0)
+        {
+            shootProjectile();
+            yield return new WaitForSeconds(shootSpeed);
+        }
+    }
+
+    protected void shootProjectile()
+    {
+        GameObject newProjectile = Instantiate(projectilePrefab, transform);
+    }
+    #endregion
+}
