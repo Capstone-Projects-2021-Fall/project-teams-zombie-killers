472f3497bbf0fa108c48e8a9ed0f30d398de6409
Francesko Racaku <franceskoracaku@hotmail.com>
2021-12-02 19:00:56 -0500

    Achievement Updates
    Added Achievement
    Fixed Achievement UI
    Added Hidden Achievement

--- Assets/Scripts/Defender/DefenderSpawner.cs
+++ Assets/Scripts/Defender/DefenderSpawner.cs
@@ -89,11 +89,15 @@ public class DefenderSpawner : MonoBehaviour, IPointerDownHandler
             occupiedSquares.Add(gridPos);
 
             AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_DEFENDER, 1);
+            AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_MASTERDEFENDER, 1);
+            AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_EXPERTDEFENDER, 1);
 
             if (defenderPrefab.name.Contains("Cactus"))
             {
 
                 AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_CACTUS, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_MASTERCACTUS, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_EXPERTCACTUS, 1);
 
             }
 
@@ -101,6 +105,8 @@ public class DefenderSpawner : MonoBehaviour, IPointerDownHandler
             {
 
                 AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_TROPHY, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_MASTERTROPHY, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_EXPERTTROPHY, 1);
 
             }
 
@@ -108,6 +114,8 @@ public class DefenderSpawner : MonoBehaviour, IPointerDownHandler
             {
 
                 AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_FOREHORSEMAN, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_MASTERFOREHORSEMAN, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_EXPERTMASTERFOREHORSEMAN, 1);
 
             }
 
@@ -115,6 +123,8 @@ public class DefenderSpawner : MonoBehaviour, IPointerDownHandler
             {
 
                 AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_LIBERTYBELL, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_MASTERLIBERTYBELL, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_EXPERTLIBERTYBELL, 1);
 
             }
 
@@ -122,6 +132,8 @@ public class DefenderSpawner : MonoBehaviour, IPointerDownHandler
             {
 
                 AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_GNOME, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_EXPERTGNOME, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_MASTERGNOME, 1);
 
             }
 
@@ -129,6 +141,8 @@ public class DefenderSpawner : MonoBehaviour, IPointerDownHandler
             {
 
                 AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_NINJA, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_EXPERTNINJA, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_MASTERNINJA, 1);
 
             }
 
@@ -136,6 +150,8 @@ public class DefenderSpawner : MonoBehaviour, IPointerDownHandler
             {
 
                 AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_ROCKY, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_EXPERTROCKY, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_MASTERROCKY, 1);
 
             }
 
@@ -143,6 +159,44 @@ public class DefenderSpawner : MonoBehaviour, IPointerDownHandler
             {
 
                 AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_FIREMAN, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_EXPERTFIREMAN, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_MASTERFIREMAN, 1);
+
+            }
+
+            else if (defenderPrefab.name.Contains("Adventure_girl"))
+            {
+
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_ADVENTUREGIRL, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_EXPERTADVENTUREGIRL, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_MASTERADVENTUREGIRL, 1);
+
+            }
+
+            else if (defenderPrefab.name.Contains("Robot"))
+            {
+
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_ROBOT, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_EXPERTADVENTUREGIRL, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_MASTERADVENTUREGIRL, 1);
+
+            }
+
+            else if (defenderPrefab.name.Contains("Freek"))
+            {
+
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_FREEK, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_EXPERTFREEK, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_MASTERFREEK, 1);
+
+            }
+
+            else if (defenderPrefab.name.Contains("Bomber"))
+            {
+
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_BOMBER, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_EXPERTBOMBER, 1);
+                AchievementManager.achievementManagerInstance.AddAchievementProgress(AchievementType.ZKS_MASTERBOMBER, 1);
 
             }
 
