95c75e0c2ee826d35a9abd784298b4e16e1ced3e
Marcus-Anestad <tuj23850@temple.edu>
2021-12-04 18:03:03 -0500

    Info Popups completed

---
+++ Assets/Scripts/InfoPopup.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPopup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(gameObject.active == true)
+            Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResumeTime()
+    {
+        Time.timeScale = 1;
+        transform.parent.parent.gameObject.SetActive(false);
+    }
+}
