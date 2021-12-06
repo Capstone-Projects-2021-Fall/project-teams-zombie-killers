86e6bc79f7e17f8cc927f87aebe88e9d44e0a16f
Darren Chen <tuj72544@temple.edu>
2021-12-05 23:04:13 -0500

    Hooked up Tutorial to Level Selection

--- Assets/Scripts/LevelSelection.cs
+++ Assets/Scripts/LevelSelection.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelection : MonoBehaviour
 {
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
     public void Level1()
     {
         SceneManager.LoadScene("Level 1");
